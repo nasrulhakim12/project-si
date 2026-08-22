1728695ffc659d1e15174c67b187c3638d8e2ca4
nasrul hakim <nasrulhakim@macbook-neo.local>
2026-08-23 04:18:22 +0700

    debug vercel php error

--- api/index.php
+++ api/index.php
@@ -1,9 +1,12 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app->handleRequest(
-    \Illuminate\Http\Request::capture()
-);
+$request = \Illuminate\Http\Request::capture();
+
+$app->handleRequest($request);
